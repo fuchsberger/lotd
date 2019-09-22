63c66d10657a2ecc9e4b53664595ec4a14fc5652
fuchsberger <admin@fuchsberger.us>
2019-09-22 18:19:36 -0400

    serve endpoints

--- config/prod.exs
+++ config/prod.exs
@@ -19,3 +19,5 @@ config :logger, level: :info
 # Finally import the config/prod.secret.exs which loads secrets
 # and configuration from environment variables.
 import_config "prod.secret.exs"
+
+config :phoenix, :serve_endpoints, true
