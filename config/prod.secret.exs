5b606100a37c3169a8b5e02a9679b993d060ec40
fuchsberger <admin@fuchsberger.us>
2019-09-22 18:13:18 -0400

    removed ipv6 config

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -24,7 +24,7 @@ secret_key_base =
     """
 
 config :lotd, LotdWeb.Endpoint,
-  http: [:inet6, port: String.to_integer(System.get_env("LOTD_PORT") || "4000")],
+  http: [port: String.to_integer(System.get_env("LOTD_PORT") || "4000")],
   secret_key_base: secret_key_base
 
 # ## Using releases (Elixir v1.9+)
