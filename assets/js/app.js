c4ca73fc72f80dc06e7e053031d36dbe80874d22
fuchsberger <admin@fuchsberger.us>
2019-09-23 15:25:14 -0400

    added turbolinks

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,6 @@
+import Turbolinks from "turbolinks"
+Turbolinks.start()
+
 // We need to import the CSS so that webpack will load it.
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
