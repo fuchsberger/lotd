75e657599e182733c009f26b25d72be83ef51b97
fuchsberger <admin@fuchsberger.us>
2019-09-27 15:25:37 -0400

    fixed some mobile issues

--- lib/lotd_web/views/layout_view.ex
+++ lib/lotd_web/views/layout_view.ex
@@ -5,23 +5,25 @@ defmodule LotdWeb.LayoutView do
     link [icon("off", class: "is-small"), content_tag(:span, "Logout")],
       to: Routes.session_path(conn, :delete, conn.assigns.current_user.id),
       method: "delete",
-      class: "button has-icons",
+      class: "button has-icons has-text-centered",
       title: "Logout #{conn.assigns.current_user.nexus_name}"
   end
 
-  def menu_link(conn, module, title) do
+  defp current_module(path_info) do
+    if path_info == [], do: nil, else: path_info |> List.first() |> String.to_atom()
+  end
 
-    current_module = if conn.path_info == [],
-      do: nil,
-      else: conn.path_info |> List.first() |> String.to_atom()
-
-    class = if current_module == module, do: "is-active", else: ""
-
-    path = case module do
+  defp get_path(conn, module) do
+    case module do
       :user -> Routes.user_path(conn, :index)
       _ -> Routes.page_path(conn, :index)
     end
+  end
 
+  def menu_link(conn, module, title, opts \\ []) do
+    active = if current_module(conn.path_info) == module, do: " is-active", else: ""
+    class = Keyword.get(opts, :class, "") <> active
+    path = get_path(conn, module)
     content_tag :li, link(title, to: path), class: class
   end
 
