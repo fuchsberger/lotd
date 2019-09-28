3799be0f0ce17a6c21ce8807e1c8c6500c78993d
fuchsberger <admin@fuchsberger.us>
2019-09-28 11:59:41 -0400

    more fluid table generation

--- assets/js/views/main.js
+++ assets/js/views/main.js
@@ -1,21 +1,48 @@
 import $ from 'jquery'
+import 'datatables.net'
+import 'timeago'
 
 import { login } from '../api/nexus'
 
 export default class MainView {
   mount() {
 
-    // Check for click events on the navbar burger icon
-    $(".navbar-burger").click(() => {
-      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-      $(".navbar-burger, .navbar-menu").toggleClass("is-active")
-    })
+    // enable mobile menu
+    $(".navbar-burger").click(() => { $(".navbar-burger, .navbar-menu").toggleClass("is-active")})
 
     // enable login button
     $('#signInBtn').click(() => login())
 
     // enable dismissing of notifications
     $('.notification button.delete').on('click', function(){ $(this).parent().remove() })
+
+    // if we have a table, create a datatable
+    if ($('table').length ) {
+
+      // enable timeago on table redraw
+      $("table").on('draw.dt', function() { $("time").timeago() })
+
+      // create datatable
+      let table = $('table').DataTable({ dom: 't', paging: false, info: false })
+
+      let info = table.page.info()
+
+      // initial display total count
+      $('.total-count').text(info.recordsTotal)
+
+      // enable searching/filtering
+      $('#search').on('keyup', function(){
+        table.search( this.value ).draw()
+        let info = table.page.info()
+
+        $('.total-count').text(info.recordsTotal)
+        $('.filtered-count')
+          .text(info.recordsTotal != info.recordsDisplay ? info.recordsDisplay + ' / ' : '')
+      })
+
+      // store table for unmount
+      this.table = table
+    }
 
     // hide loader
     $('#loader-wrapper').addClass('is-hidden')
@@ -29,5 +56,6 @@ export default class MainView {
     sessionStorage.lastView = $('body').data('view')
     sessionStorage.scrollTop = $(window).scrollTop()
 
+    if(this.table) this.table.destroy()
   }
 }
