4ed7d9f0228e3f71743c191146d23fe3f537f2cd
fuchsberger <admin@fuchsberger.us>
2019-09-30 19:34:00 -0400

    bumped version 1.4

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -24,7 +24,8 @@ secret_key_base =
 
 config :lotd, LotdWeb.Endpoint,
   http: [port: String.to_integer(System.get_env("LOTD_PORT") || "4000")],
-  secret_key_base: secret_key_base
+  secret_key_base: secret_key_base,
+  debug_errors: true # TODO: don't forget to disable after troubleshooting
 
 # ## Using releases (Elixir v1.9+)
 #
