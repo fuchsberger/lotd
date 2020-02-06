5bda5492c65e4c1c6cf8742cb373020375472fa1
fuchsberger <admin@fuchsberger.us>
2020-02-05 21:38:56 -0500

    preparing new gallery page

--- lib/lotd_web/live/items_live.ex
+++ lib/lotd_web/live/items_live.ex
@@ -1,7 +1,22 @@
 defmodule LotdWeb.ItemsLive do
-  use Phoenix.LiveView, container: {:div, class: "container"}
+  use Phoenix.LiveView, container: {:div, class: "container pt-2"}
 
   alias Lotd.{Accounts, Gallery}
+
+  @defaults [
+    # character_id: nil,
+    # character_items: nil,
+    # changeset: nil,
+    # display_filter: nil,
+    # hide: false,
+    # location_filter: nil,
+    moderate: false,
+    moderator: false,
+    # mod_filter: nil,
+    # room_filter: nil,
+    # search: "",
+    # user: nil
+  ]
 
   def render(assigns), do: LotdWeb.ItemView.render("index.html", assigns)
 
@@ -12,11 +27,26 @@ defmodule LotdWeb.ItemsLive do
       do: Gallery.list_items(),
       else: Gallery.list_items(user.active_character.mods)
 
-    {:ok, assign(socket, :items, items)}
+      assigns = [
+        items: items,
+        moderator: user.moderator
+      ]
+
+      {:ok, assign(socket, Keyword.merge(@defaults, assigns))}
   end
 
   def mount(_params, _session, socket) do
     items = Gallery.list_items()
-    {:ok, assign(socket, :items, items)}
+
+    assigns = [
+      items: items,
+    ]
+
+    {:ok, assign(socket, Keyword.merge(@defaults, assigns))}
+  end
+
+  def handle_event("toggle", %{"field" => field}, socket) do
+    field = String.to_atom(field)
+    {:noreply, assign(socket, field, !Map.get(socket.assigns, field))}
   end
 end
--- lib/lotd_web/views/item_view.ex
+++ lib/lotd_web/views/item_view.ex
@@ -1,4 +1,10 @@
 defmodule LotdWeb.ItemView do
   use LotdWeb, :view
 
+
+  def check_item(moderator) do
+    if moderator,
+      do: icon("edit", class: "text-primary", phx_click: "toggle", phx_value_field: "moderate"),
+      else: icon("active")
+  end
 end
